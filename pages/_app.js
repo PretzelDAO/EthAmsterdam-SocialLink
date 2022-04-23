ab61e81d412d45d8b5be7592b1b5a8a4a2ef0689
Vitarico <victoria.hauzeneder@gmail.com>
2022-04-23 11:46:30 +0200

    adjusted global colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,36 @@
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import Header from '../components/Header'
-import UserContext from '../contexts/UserContext'
-import '../styles/globals.css'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import UserContext from "../contexts/UserContext";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const [state, setState] = useState({
     user: null,
     profileId: null,
     update,
-  })
+  });
   function update(data) {
-    let updateData = data
+    let updateData = data;
     Object.keys(state).forEach(function (key) {
       if (key in data) {
         // or obj1.hasOwnProperty(key)
-        updateData[key] = data[key]
+        updateData[key] = data[key];
       } else {
-        updateData[key] = state[key]
+        updateData[key] = state[key];
       }
-    })
-    console.log('updating state', updateData, state, data)
-    setState(Object.assign({}, state, updateData))
+    });
+    console.log("updating state", updateData, state, data);
+    setState(Object.assign({}, state, updateData));
   }
   useEffect(() => {
     // import('tw-elements')
-  }, [])
+  }, []);
 
   return (
     <UserContext.Provider value={state}>
       <div className=" max-w-full max-h-full h-full min-h-screen  bg-grape-blue-900 text-gray-700 font-opensans flex justify-center  text-base font-semibold">
-        <div className="flex items-center flex-col w-screen bg-gray-100">
+        <div className="flex items-center flex-col w-screen bg-background-200">
           <Header />
           {/* <Link
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -49,7 +49,7 @@ function MyApp({ Component, pageProps }) {
         </div>
       </div>
     </UserContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
