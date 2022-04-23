86b153cb1a84e841f0a3614fab63f82f77044445
Totenfluch <github@cziegler.de>
2022-04-23 21:38:35 +0200

    ui fixes

--- components/SocialProfileCard.js
+++ components/SocialProfileCard.js
@@ -9,13 +9,13 @@ export default function SocialProfileCard({ icon, link, state }) {
   return (
     <div className="border border-gray-300 mx-2 py-2 my-2 flex flex-col items-center w-fit px-4 rounded">
       <div>
-        <span className="py-2">{icon}</span>
+        <div className="py-2 align-middle self-center mx-auto w-16">{icon}</div>
         <BLPButton
           text={'Connect'}
           disabled={state == 'disabled'}
           clickaction={async () => {
             console.log('lol', sharedAccountContract)
-            window.open(link, '_blank')
+            window.open(link, '_blank');
             await setProfileLensDispatcher(
               global.profileId,
               sharedAccountContract,
