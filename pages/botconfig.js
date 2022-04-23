0505f69932a219444f8de0373d94f9c1e962a15d
Vitarico <victoria.hauzeneder@gmail.com>
2022-04-23 14:43:09 +0200

    styling lens connect

--- pages/botconfig.js
+++ pages/botconfig.js
@@ -1,19 +1,19 @@
-import { useContext, useState } from 'react'
-import BotConnect from '../components/BotConnect'
-import ConnectButton from '../components/ConnectButton'
-import ConnectProfile from '../components/ConnectProfile'
-import CreateProfileModal from '../components/CreateProfileModal'
-import UserContext from '../contexts/UserContext'
+import { useContext, useState } from "react";
+import BotConnect from "../components/BotConnect";
+import ConnectButton from "../components/ConnectButton";
+import ConnectProfile from "../components/ConnectProfile";
+import CreateProfileModal from "../components/CreateProfileModal";
+import UserContext from "../contexts/UserContext";
 
 export default function BotCheck() {
   // const [handle, setHandle] = useState()
-  const global = useContext(UserContext)
+  const global = useContext(UserContext);
   // const history = useHistory();
-  let toshow
+  let toshow;
   if (global.profileId) {
-    console.log('logged in')
+    console.log("logged in");
     // history.push('/')
-    toshow = <BotConnect />
+    toshow = <BotConnect />;
   } else {
     toshow = global.user ? (
       <div className="flex flex-row ">
@@ -22,12 +22,26 @@ export default function BotCheck() {
       </div>
     ) : (
       <ConnectButton />
-    )
+    );
   }
 
   return (
-    <div className="w-full flex justify-center flex-col">
-      <div className="min-w-md max-w-[80vw] rounded-md overflow-hidden shadow-2xl w-5/6 m-2 bg-white flex flex-col items-start py-3 font-light px-4">
+    <div className="w-full flex justify-center my-20">
+      <div
+        className="pl-8 min-w-md max-w-[50vw]
+                      rounded-md
+                      overflow-hidden
+                      shadow-2xl
+                      w-5/6
+                      bg-white
+                      flex
+                      flex-col
+                      items-start
+                      space-y-4
+                      py-4
+                      font-light
+                      px-4"
+      >
         <span className="py-3 text-lg">Bot: SocialLink#1224</span>
         <div className="font-light py-3">
           Please enter your on-chain Profile:
@@ -35,5 +49,5 @@ export default function BotCheck() {
         {toshow}
       </div>
     </div>
-  )
+  );
 }
